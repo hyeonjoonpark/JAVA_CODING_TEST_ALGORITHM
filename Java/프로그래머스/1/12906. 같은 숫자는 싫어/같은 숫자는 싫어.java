78f94f78bf33ea194c778553193eaa92ff54c13a
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2025-02-20 22:15:08 +0900

    Update 같은 숫자는 싫어.java

--- Java/프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ Java/프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Solution {
     public int[] solution(int []arr) {
         Stack<Integer> stack = new Stack<>();
-        List<Integer> result = new ArrayList<>();
         stack.push(arr[0]);
 
         for (int i = 1; i < arr.length; i++) {
